dca19c0b53c23b2eda20278a94cb5999fa76a1fb
Saurabh patil <sp3667656@gmail.com>
2026-08-13 17:56:58 +0530

    Solve leaky-relu

--- leaky-relu/leaky-relu.py
+++ leaky-relu/leaky-relu.py
@@ -5,12 +5,6 @@ def leaky_relu(x, alpha=0.01):
     Vectorized Leaky ReLU implementation.
     """
     # Write code here
-    x = np.array(x)
-    leaky_list = []
-    for i in range(len(x)):
-        if x[i]<0:
-            leaky_list.append(alpha*x[i])
-        else:
-            leaky_list.append(x[i])
-    return np.array(leaky_list)
+    x = np.asarray(x)
+    return np.where(x < 0, alpha * x, x)
             
